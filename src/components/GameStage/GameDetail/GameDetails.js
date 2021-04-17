2a09217c96a4e0e56026ea1157d3ef9894cf9490
Andrew Watson <andrew_tyler_watson@yahoo.com>
2021-04-17 01:35:51 -0400

    Added a new package to handle asynchronous effects in component lifecycles. Added a useEffect hook to fill games state in layout with data from MocsArcade API This currently only works with a branch of the MocsArcade API that supports CORS GameCarousel now displays games from API

--- src/components/GameStage/GameDetail/GameDetails.js
+++ src/components/GameStage/GameDetail/GameDetails.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const GameDetailsPanel = () => {
-  return <div>Game Details Panel</div>;
+const GameDetails = (props) => {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+    </div>
+  );
 };
+
+export default GameDetails;
